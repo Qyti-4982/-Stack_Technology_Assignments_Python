b17584457547768d0615c7d7673d8dd037954dd7
Qyti-4982 <ijmoknerch52wtish47@mail.ru>
2025-11-14 20:23:42 +0300

    Add files via upload

---
+++ Урок №3. Ввод-вывод и базовые переменные/Урок №3. Задание 2.py
@@ -0,0 +1,7 @@
+
+first = input("Первый этап эволюции человека:")
+second = input("Второй этап эволюции человека:")
+third = input("Третий этап эволюции человека:")
+fourth = input("Четвёртый этап эволюции человека:")
+print(first, second, third, fourth, sep = "=>")
+

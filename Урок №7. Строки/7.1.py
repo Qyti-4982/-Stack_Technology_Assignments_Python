60a5ebbd9e8c41b72b0be85866a21635b817357e
Qyti-4982 <ijmoknerch52wtish47@mail.ru>
2025-11-14 22:35:34 +0300

    Add files via upload

---
+++ Урок №7. Строки/7.1.py
@@ -0,0 +1,7 @@
+
+str_1= input("Введите строку:")
+str_2 = str_1[::-1]
+if str_1 == str_2:
+    print("Yes")
+else:
+    print("No")
---
+++ Урок №7. Строки/7.2.py
@@ -0,0 +1,4 @@
+
+str_1 = input("Введите строку: ").strip()
+str_2 = " ".join(str_1.split())
+print(str_2)
